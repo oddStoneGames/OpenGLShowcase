8565b7e41e8ef033773aacb453f308b9f71d7211
oddstonegames <oddstonegames@gmail.com>
2023-01-17 15:09:23 +0530

    Added GLAD & GLFW & Hello Window

---
+++ src/main.cpp
@@ -0,0 +1,39 @@
+#include "../vendor/glad/include/glad.h"
+#include "../vendor/glfw/include/glfw3.h"
+#include <iostream>
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Showcase", NULL, NULL);
+    if(window == NULL)
+    {
+        std::cout << "Failed to create GLFW Window! \n Exiting... \n";
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to Initialize GLAD! \n Exiting... \n";
+        glfwTerminate();
+        return -1;
+    }
+
+    
+    while(!glfwWindowShouldClose(window))
+    {
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
